79431db9ed191c35dbff664b525daabb3836cc75
defenstration <david.blackman.career@gmail.com>
2024-07-15 17:36:35 -0400

    start of project

---
+++ script.mjs
@@ -0,0 +1,5 @@
+const url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
+
+d3.select('body')
+    .append('h1')
+    .text("fCC d3 Scatterplot Graph")
